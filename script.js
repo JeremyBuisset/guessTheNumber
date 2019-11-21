58e20b0333453360ff50057a47aa229ae972bd81
JeremyBuisset <56023195+JeremyBuisset@users.noreply.github.com>
2019-11-21 16:11:45 +0100

    Update

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ var essais = 10;
 //Random Number
 var random;
 var play;
-var gameover;
 var dejaVu;
+var gameover;
 
 //Function for the generation of random numbers
 function hasard (){
     random = Math.floor(Math.random() * (101 - 1)) + 1;
 }
 //Function for remove "alert" window
-function display() {
+function remove() {
     document.getElementById("resultat").style.display = "none";
 }
 
@@ -43,38 +43,36 @@ document.getElementById("game").addEventListener("click",function () {
     } else if (user == random) {
         document.getElementById("display").innerHTML = "C'est gagné !!!";
         document.getElementById("display").style.backgroundColor = "green";
-        gameover = true;
+        document.getElementById("replay").style.display = "inline-block";
     }
     if (essais < 1) {
         document.getElementById("resultat").style.display = "block";
         document.getElementById("contResultat").innerHTML = "C'est perdu !!! Vous pouvez rejouer en utilisant le bouton rejouer";
-        gameover = true;
-        document.getElementById('replay').style.backgroundColor = 'cornflowerblue';
+        document.getElementById("replay").style.display = "inline-block";
+        setTimeout(remove,2000)
     }
 });
 
 document.getElementById("replay").addEventListener("click",function () {
-    if (gameover == true){
-        document.getElementById("resultat").style.display = "block";
-        document.getElementById("contResultat").innerHTML = "Une nouvelle partie va débuter, bonne chance !";
-        document.getElementById("resultat").addEventListener("click", function () {
-            document.getElementById("resultat").style.display = "none";
-            document.getElementById('replay').style.backgroundColor = 'white';
-            essais = 10;
-            document.getElementById("affi").innerHTML = essais;
-            document.getElementById("display").innerHTML = "";
-            document.getElementById('display').style.backgroundColor = 'white';
-            document.getElementById("choix").value = "";
-            document.getElementById("deja").innerHTML = "Vous avez déjà tenté les valeurs suivantes :";
-            hasard();
-            gameover = false;
-        })
-    } else {
-        document.getElementById("resultat").style.display = "block";
-        document.getElementById("contResultat").innerHTML = "Vous devez avoir terminé une partie pour pouvoir rejouer";
-        setTimeout(display,2000);
-    }
-
+    document.getElementById("resultat").style.display = "block";
+    document.getElementById("contResultat").innerHTML = "Une nouvelle partie va débuter, bonne chance !";
+    setTimeout(remove,2000);
+    document.getElementById('replay').style.backgroundColor = 'white';
+    essais = 10;
+    document.getElementById("affi").innerHTML = essais;
+    document.getElementById("display").innerHTML = "";
+    document.getElementById('display').style.backgroundColor = 'white';
+    document.getElementById("choix").value = "";
+    document.getElementById("deja").innerHTML = "Vous avez déjà tenté les valeurs suivantes :";
+    document.getElementById("replay").style.display = "none";
+    hasard();
 });
+
+
+
+
+
+
+
 
 
